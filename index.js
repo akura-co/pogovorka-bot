b1e0f854c8bb2c6281f2bfd150ee95e5d92631ab
Afanasii Kurakin <afanasy@gmail.com>
2016-02-27 13:01:18 +0800

    added inline mode and query filter support

--- index.js
+++ index.js
@@ -10,11 +10,32 @@ module.exports = function () {
     else
       console.log('setWebhook ok', config.setWebhook)
   })
+  var filter = function (q) {
+    var data = _.filter(config.proverb, function (d) {return d.toLowerCase().indexOf(q.toLowerCase()) != -1})
+    if (!data.length)
+      data = config.proverb
+    return data
+  }
   return function (req, res) {
-    var proverb = _.filter(config.proverb, function (d) {return d.toLowerCase().indexOf(req.body.message.toLowerCase()) != -1})
-    if (!proverb.length)
-      proverb = config.proverb
-    console.log(req.body, _.sample(proverb))
-    res.json({method: 'sendMessage', chat_id: req.body.message.chat.id, text: _.sample(proverb)})
+    console.log(req.body)
+    var json = {}
+    if (req.body.inline_query) {
+      json = {
+        method: 'answerInlineQuery',
+        inline_query_id: req.body.inline_query.id,
+        results: JSON.stringify(_.map(filter(req.body.inline_query.query), function (d, i) {
+          return {
+            type: 'article',
+            id: String(i),
+            title: d,
+            message_text: d
+          }
+        }))
+      }
+    }
+    if (req.body.message)
+      json = {method: 'sendMessage', chat_id: req.body.message.chat.id, text: _.sample(filter(req.body.message.text))}
+    console.log(json)
+    res.json(json)
   }
 }
