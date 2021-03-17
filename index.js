12baf553ae600dcacd566da8cd1de93828ae0665
Afanasii Kurakin <afanasy@gmail.com>
2021-03-17 13:39:33 +0300

    updated code and deps

--- index.js
+++ index.js
@@ -1,36 +1,27 @@
-var
-  _ = require('underscore'),
-  request = require('request'),
-  config = require('solid-config')
+var _ = require('underscore')
+var package = require('./package')
+var config = package[package.name]
 
-module.exports = function () {
-  request({url: config.url + config.token + '/setWebhook', qs: {url: config.setWebhook}}, function (err) {
-    if (err)
-      console.log(err)
-    else
-      console.log('setWebhook ok', config.setWebhook)
-  })
-  var filter = function (q) {
-    var data = _.filter(config.proverb, function (d) {return d.toLowerCase().indexOf(q.toLowerCase()) != -1})
-    if (!data.length)
-      data = config.proverb
-    return data
+module.exports = () => {
+  function filter (q) {
+    var data = _.filter(config.proverb, d => d.toLowerCase().indexOf(q.toLowerCase()) != -1)
+    if (_.size(data))
+      return data
+    return config.proverb
   }
-  return function (req, res) {
+  return (req, res) => {
     console.log(req.body)
     var json = {}
     if (req.body.inline_query) {
       json = {
         method: 'answerInlineQuery',
         inline_query_id: req.body.inline_query.id,
-        results: JSON.stringify(_.map(filter(req.body.inline_query.query), function (d, i) {
-          return {
-            type: 'article',
-            id: String(i),
-            title: d,
-            message_text: d
-          }
-        }))
+        results: JSON.stringify(_.map(filter(req.body.inline_query.query), (d, i) => ({
+          type: 'article',
+          id: String(i),
+          title: d,
+          message_text: d
+        })))
       }
     }
     if (req.body.message)
